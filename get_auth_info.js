cd37a9c259b2522a7f20de1e0856bbb8950d99a1
henrique <henriquevt98@gmail.com>
2024-02-16 11:17:32 -0300

    Added `get_auth_info.js`

---
+++ get_auth_info.js
@@ -0,0 +1,2 @@
+// Dead simple fetch, used for grabbing auth_info from localStorage to quickly copy onto local mock auth
+javascript:(function(){var authInfo=localStorage.getItem('auth_info');if(authInfo){navigator.clipboard.writeText(authInfo);alert('auth_info copied to clipboard!');}else{alert('auth_info not found in localStorage!');}})();
